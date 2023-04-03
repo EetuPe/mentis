fe46f4ee58c0b98d0ff8885bcb682d1859038ac3
EetuPe <eetu.petanen@gmail.com>
2023-04-03 23:12:31 +0300

    Color picker on side

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,23 @@ import WordRow from "./WordRow";
 const GUESS_LENGTH = 10;
 export default function App() {
   const state = useStore();
-  const [guess, setGuess] = useState("");
+  const [finalGuess, setFinalGuess] = useState("");
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newGuess = e.target.value;
 
-    if (newGuess.length === CODE_LENGTH) {
-      state.addGuess(newGuess);
-      setGuess("");
+    if (finalGuess.length === CODE_LENGTH) {
+      state.addGuess(finalGuess);
+      setFinalGuess("");
       return;
     }
-    setGuess(newGuess);
+    setFinalGuess(finalGuess + newGuess);
   };
 
   let rows = [...state.guesses];
 
   if (rows.length < GUESS_LENGTH) {
-    rows.push(guess);
+    rows.push(finalGuess);
   }
 
   const numberOfGuessesRemaining = GUESS_LENGTH - rows.length;
@@ -41,18 +41,18 @@ export default function App() {
         <h1 className="text-4xl text-center text-neutral-50">Mentis</h1>
 
         <div>
-          <input
+          {/* <input
             type="text"
             className="w-full rounded p-2 border-2 border-gray-500"
-            value={guess}
+            value={finalGuess}
             onChange={onChange}
             disabled={isGameOver}
-          />
+          /> */}
           <button
             className="block border rounded border-green-500 bg-green-500 p-2 mt-4 mx-auto shadow text-white"
             onClick={() => {
               state.newGame();
-              setGuess("");
+              setFinalGuess("");
             }}
           >
             New Game
@@ -64,6 +64,64 @@ export default function App() {
         {rows.reverse().map((word, index) => (
           <WordRow key={index} letters={word} colors="" guessStates={[]} />
         ))}
+      </div>
+      <div className="grid grid-cols-1 p-2">
+        <button
+          className={`btn border-2 bg-red-500 border-red-500 w-full h-full rounded font-bold text-lg align-middle text-neutral-50`}
+          onClick={onChange}
+          value="0"
+        >
+          0
+        </button>
+        <button
+          className={`btn border-2 bg-sky-400 border-sky-400 w-full h-full rounded font-bold text-lg align-middle text-neutral-50`}
+          onClick={onChange}
+          value="1"
+        >
+          1
+        </button>
+        <button
+          className={`btn border-2 bg-lime-500 border-lime-500 w-full h-full rounded font-bold text-lg align-middle text-neutral-50`}
+          onClick={onChange}
+          value="2"
+        >
+          2
+        </button>
+        <button
+          className={`btn border-2 bg-yellow-400 border-yellow-400 w-full h-full rounded font-bold text-lg align-middle text-neutral-50`}
+          onClick={onChange}
+          value="3"
+        >
+          3
+        </button>
+        <button
+          className={`btn border-2 bg-purple-500 border-purple-500 w-full h-full rounded font-bold text-lg align-middle text-neutral-50`}
+          onClick={onChange}
+          value="4"
+        >
+          4
+        </button>
+        <button
+          className={`btn border-2 bg-orange-500 border-orange-500 w-full h-full rounded font-bold text-lg align-middle text-neutral-50`}
+          onClick={onChange}
+          value="5"
+        >
+          5
+        </button>
+        <button
+          className={`btn border-2 bg-teal-500 border-teal-500 w-full h-full rounded font-bold text-lg align-middle text-neutral-50`}
+          onClick={onChange}
+          value="6"
+        >
+          6
+        </button>
+        <button
+          className={`btn border-2 bg-pink-500 border-pink-500 w-full h-full rounded font-bold text-lg align-middle text-neutral-50`}
+          onClick={onChange}
+          value="7"
+        >
+          7
+        </button>
       </div>
     </body>
   );
